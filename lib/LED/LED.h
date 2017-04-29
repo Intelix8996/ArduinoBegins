da6e20e61d90b958c1a4328a3e4a55bf7bebdc7f
Intelix8996 <nikolat-repin201303@mail.ru>
2017-04-29 16:26:31 +0700

    Added LED library

---
+++ lib/LED/LED.h
@@ -0,0 +1,16 @@
+#ifndef LED_h
+#define LED_h
+
+#include "Arduino.h"
+
+#define LEDBOARD 13
+
+#define BoardLEDOn digitalWrite(LEDBOARD, HIGH)
+#define BoardLEDOff digitalWrite(LEDBOARD, LOW)
+
+void On (int pinNum);
+
+void Off (int pinNum);
+
+#endif
+
